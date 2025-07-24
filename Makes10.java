703db8e0da1a9d85333cc608f9e1b28b296faecb
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-24 14:19:27 +0400

    Q7 Makes10 Solved

---
+++ Makes10.java
@@ -0,0 +1,13 @@
+public class Makes10 {
+    public boolean makes10(int a, int b) {
+        if(a == 10 || b==10){
+            return true;
+        }
+        if(a+b == 10){
+            return true;
+        }
+
+        return false;
+    }
+
+}
