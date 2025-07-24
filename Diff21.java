efefcb9240f41ddb9a3234d64091c7244f6ec41f
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-24 14:14:11 +0400

    Q4 diff21 Solved

---
+++ Diff21.java
@@ -0,0 +1,13 @@
+public class Diff21 {
+    public static  int  diff21(int n) {
+        if(n<=21){
+            return 21-n;
+        }
+        return (n-21)*2;
+    }
+
+    public static void main(String[] args){
+        System.out.println(diff21(22));
+    }
+
+}
