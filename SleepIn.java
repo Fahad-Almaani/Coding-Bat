c431cc2bad8f72e29bc8ff650e581a7aa1234e67
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-24 14:03:16 +0400

    Q1 SleepIn Solved

---
+++ SleepIn.java
@@ -0,0 +1,11 @@
+public class SleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation){
+        if(!weekday && !vacation){
+            return true;
+        }
+        if(weekday && !vacation){
+            return false;
+        }
+        return true;
+    }
+}
