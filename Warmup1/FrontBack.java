3e88ec9558ababe2ef1ef1bc727c373703db075c
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-25 16:43:51 +0400

    Q28 Warmup-1 FrontBack Solved

---
+++ Warmup1/FrontBack.java
@@ -0,0 +1,12 @@
+package Warmup1;
+
+public class FrontBack {
+    public String frontBack(String str) {
+        if(str.length()<=1){
+            return str;
+        }
+
+        return str.charAt(str.length()-1) + str.substring(1,str.length()-1) + str.charAt(0);
+    }
+
+}
