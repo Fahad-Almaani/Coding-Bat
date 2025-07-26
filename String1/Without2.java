0f273a3d25a77ea3c7c570f61dd81d44377a743b
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-26 08:56:20 +0400

    Q69 String1 Without2 Solved

---
+++ String1/Without2.java
@@ -0,0 +1,12 @@
+package String1;
+
+public class Without2 {
+    public String without2(String str){
+        if(str.length()<2) return str;
+        if(str.length()==2) return "";
+        if(str.substring(0,2).equals(str.substring(str.length()-2))){
+            return str.substring(2);
+        }
+        return str;
+    }
+}
