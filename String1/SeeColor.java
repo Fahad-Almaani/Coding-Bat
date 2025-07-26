90879713690e5fd64977d3a2ad1f00375aee50b8
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-26 06:10:28 +0400

    Q65 String1 SeeColor Solved

---
+++ String1/SeeColor.java
@@ -0,0 +1,10 @@
+package String1;
+
+public class SeeColor {
+    public String seeColor(String str) {
+        if(str.startsWith("red")) return "red";
+        if(str.startsWith("blue")) return "blue";
+        return "";
+    }
+
+}
