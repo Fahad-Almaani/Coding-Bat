12299aad74c467a1e5f8a88d28c4368082b92af6
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-26 23:03:51 +0400

    Q76 Array-1 MakePi Solved

---
+++ Array1/MakePi.java
@@ -0,0 +1,12 @@
+package Array1;
+
+public class MakePi {
+    public int[] makePi(){
+        int[] pi = new int[3];
+        pi[0]= 3;
+        pi[1]= 1;
+        pi[2] = 4;
+        return pi;
+
+    }
+}
