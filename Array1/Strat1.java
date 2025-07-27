706dce22a5bda1aeb1d0e1fee448ee84838e86b0
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-27 11:50:50 +0400

    Q90 Array-1 Start1 Solved

---
+++ Array1/Strat1.java
@@ -0,0 +1,17 @@
+package Array1;
+
+public class Strat1 {
+    public int start1(int[] a, int[] b) {
+        int res = 0;
+        if(a.length > 0){
+
+        if(a[0] == 1) res++;
+        }
+        if(b.length>0){
+        if(b[0]==1) res++;
+
+        }
+        return res;
+    }
+
+}
