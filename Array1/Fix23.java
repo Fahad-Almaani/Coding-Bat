87425d1127a6802b719c1ff4756cc7c61b75c428
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-27 11:47:36 +0400

    Q89 Array-1 Fix23 Solved

--- Array1/Fix23.java
+++ Array1/Fix23.java
@@ -1,4 +1,13 @@
 package Array1;
 
 public class Fix23 {
+    public int[] fix23(int[] nums){
+        for(int i=0;i<2;i++){
+            if(nums[i] == 2 && nums[i+1] == 3){
+                nums[i+1] = 0;
+                return nums;
+            }
+        }
+        return nums;
+    }
 }
