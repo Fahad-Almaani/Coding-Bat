ec70d3fcdfd454a6cab3ac99a0364b5086f658eb
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-27 13:23:04 +0400

    Q101 Logic-1 CigarParty Solved

---
+++ Logic1/CigarParty.java
@@ -0,0 +1,11 @@
+package Logic1;
+
+public class CigarParty {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if(isWeekend){
+            return cigars>=40;
+        }
+        return cigars>=40 && cigars<=60;
+    }
+
+}
