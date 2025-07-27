3ff8e1c4f9c3e5e5a218007f9be4e5ea909d69ac
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-27 14:20:22 +0400

    Q114 Logic-1 TeenSum Solved

---
+++ Logic1/TeenSum.java
@@ -0,0 +1,10 @@
+package Logic1;
+
+public class TeenSum {
+    public int teenSum(int a, int b) {
+        if(13 <= a && a <=19) return 19;
+        if(13 <= b && b <=19) return 19;
+        return a+b;
+    }
+
+}
