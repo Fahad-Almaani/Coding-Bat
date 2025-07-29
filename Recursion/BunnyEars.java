79bf71d7a9512968faae592b49f473f6b852bbf6
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-29 10:39:59 +0400

    update BunnyEars

--- Recursion/BunnyEars.java
+++ Recursion/BunnyEars.java
@@ -2,6 +2,7 @@ package Recursion;
 
 public class BunnyEars {
     public int bunnyEars(int bunnies) {
+        if(bunnies == 0) return 0;
         if(bunnies == 1) return 2;
 
         return 2 + bunnyEars(bunnies-1);
