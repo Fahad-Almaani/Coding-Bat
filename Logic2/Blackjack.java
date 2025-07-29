f19e13c21acbcde17ceb97f477e7a2a354550579
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-07-29 18:50:13 +0400

    Q138 Logic-2 Blackjack

---
+++ Logic2/Blackjack.java
@@ -0,0 +1,14 @@
+package Logic2;
+
+public class Blackjack {
+    public int blackjack(int a, int b) {
+        if(a > 21 && b>21) return 0;
+        if(a>21) return b;
+        if(b>21) return a;
+        if(Math.abs(a-21) < Math.abs(b-21)) return a;
+        return b;
+
+
+     }
+
+}
